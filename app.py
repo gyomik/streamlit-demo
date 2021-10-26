6388facc780492cf2ad6a808798a412d4ffc9278
gyomik <93223213+gyomik@users.noreply.github.com>
2021-10-26 22:37:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ share = st.text_input("Input share to check", "GOOG")
 
 data = yf.download(share,start="2021-10-01",end="2021-10-10")
 
-clicked = st.button("Click me")
-st.plotly_chart(data.Open)
+#clicked = st.button("Click me")
+st.chart_area(data.Open)
 
 data
 
