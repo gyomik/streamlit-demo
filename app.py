bda1a30dabc366cd0b1db7776fa66317f6335101
gyomik <93223213+gyomik@users.noreply.github.com>
2021-10-26 22:15:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import yfinance as yf
 
+user_input = st.text_input("Input share to check", "GOOG")
+
 data = yf.download("GOOG",start="2021-10-01",end="2021-10-10")
 
+data
+
+"""
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
+---
+"""
 
-data
