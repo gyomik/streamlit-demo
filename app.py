f7356ba772c79fb0879929255ebeae5810da93de
gyomik <93223213+gyomik@users.noreply.github.com>
2021-10-26 22:54:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ import streamlit as st
 
 share = st.text_input("Input share to check", "GOOG")
 date_start = st.date_input('start date')
-print(date_start)
 
 data = yf.download(share,start="2021-10-01",end="2021-10-10")
 
 #clicked = st.button("Click me")
 st.area_chart(data.Open)
 
-data
+date_start
 
