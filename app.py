aa8f52ed8d972589054dde67a59c93be15d7193b
gyomik <93223213+gyomik@users.noreply.github.com>
2021-10-26 22:32:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ share = st.text_input("Input share to check", "GOOG")
 
 data = yf.download(share,start="2021-10-01",end="2021-10-10")
 
-st.area_chart(data.Open)
+st.area_chart(data.Open-data.Close)
 
 data
 
