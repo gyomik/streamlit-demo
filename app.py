3307a3b9868893ab3f07513112684b3b0a3603c5
gyomik <93223213+gyomik@users.noreply.github.com>
2021-10-26 22:17:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ data = yf.download(share,start="2021-10-01",end="2021-10-10")
 
 data
 
-"""
+'''
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
-"""
+'''
 
