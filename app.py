7c61deb09be07ac9f1e0a67c87fb1338c435f0e9
gyomik <93223213+gyomik@users.noreply.github.com>
2021-10-26 22:16:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import pandas as pd
 import yfinance as yf
 import streamlit as st
 
-user_input = st.text_input("Input share to check", "GOOG")
+share = st.text_input("Input share to check", "GOOG")
 
-data = yf.download("GOOG",start="2021-10-01",end="2021-10-10")
+data = yf.download(share,start="2021-10-01",end="2021-10-10")
 
 data
 
@@ -13,6 +13,5 @@ df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
----
 """
 
