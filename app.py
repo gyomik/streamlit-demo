51a054d23e80d48d73d6c75191be69da3fd871dc
gyomik <93223213+gyomik@users.noreply.github.com>
2021-10-26 22:58:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import pandas as pd
 import yfinance as yf
 import streamlit as st
 
-st.write("Showing share data for",share,"between",\
-         str(date_start),"and",str(date_end))
-
 share = st.text_input("Input share to check", "GOOG")
 date_start = st.date_input('start date')
 date_end = st.date_input('end date')
@@ -13,4 +10,7 @@ data = yf.download(share,start=date_start,end=date_end)
 
 #clicked = st.button("Click me")
 st.area_chart(data.Open)
+
+st.write("Showing share data for",share,"between",\
+         str(date_start),"and",str(date_end))
 
