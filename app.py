a774c9f385e0237f85b28551af758bde37232192
gyomik <93223213+gyomik@users.noreply.github.com>
2021-10-26 22:12:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import yfinance as yf
+
+data = yf.download("GOOG",start="2021-01-01",end="2021-10-10")
 
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
 
-df
+data
