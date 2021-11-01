b76931310d7dccab8136f8c7fd5520c07915078c
gyomik <93223213+gyomik@users.noreply.github.com>
2021-11-01 13:33:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ df = pd.DataFrame(
 deck = pdk.Deck(map_style='mapbox://styles/mapbox/light-v9',
                 initial_view_state=pdk.ViewState(latitude=37.76,longitude=-122.4,zoom=11,pitch=50,)
                 
-layers=[pdk.Layer('HexagonLayer',data=df,get_position='[lon, lat]',radius=200,elevation_scale=4,elevation_range=[0, 1000],pickable=True,extruded=True,),
+ly=[pdk.Layer('HexagonLayer',data=df,get_position='[lon, lat]',radius=200,elevation_scale=4,elevation_range=[0, 1000],pickable=True,extruded=True,),
         pdk.Layer('ScatterplotLayer',data=df,get_position='[lon, lat]',get_color='[200, 30, 0, 160]',get_radius=200,)]
                 
 st.pydeck_chart(deck,ly)
